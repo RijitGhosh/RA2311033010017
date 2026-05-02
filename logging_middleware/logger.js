6a8759e1610b957cd806b8cea35e7b1aec9bca96
Rijit Ghosh <rijitghosh53@gmail.com>
2026-05-02 12:23:26 +0530

    Update logger.js

--- logging_middleware/logger.js
+++ logging_middleware/logger.js
@@ -1,10 +1,39 @@
 const axios = require("axios");
 
-const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiJyaWppdGdob3NoNTNAZ21haWwuY29tIiwiZXhwIjoxNzc3NzA1NTM2LCJpYXQiOjE3Nzc3MDQ2MzYsImlzcyI6IkFmZm9yZCBNZWRpY2FsIFRlY2hub2xvZ2llcyBQcml2YXRlIExpbWl0ZWQiLCJqdGkiOiIzMWUyZjM4Ni03ZWMyLTQ2NGEtOGNhOC05M2RmMGVhNTAxNWIiLCJsb2NhbGUiOiJlbi1JTiIsIm5hbWUiOiJyaWppdCBnaG9zaCIsInN1YiI6IjhmZmNkMGYzLTQyNTYtNGU2MS05NjcxLTUwNWQwYjBmOGM4NSJ9LCJlbWFpbCI6InJpaml0Z2hvc2g1M0BnbWFpbC5jb20iLCJuYW1lIjoicmlqaXQgZ2hvc2giLCJyb2xsTm8iOiJyYTIzMTEwMzMwMTAwMTciLCJhY2Nlc3NDb2RlIjoiUWticHhIIiwiY2xpZW50SUQiOiI4ZmZjZDBmMy00MjU2LTRlNjEtOTY3MS01MDVkMGIwZjhjODUiLCJjbGllbnRTZWNyZXQiOiJxQXdmWkFxSHFaVmZYbnhWIn0.NLcp9qzFI0LXMX8peY_CA2Dpg9ChU1RGw2-foY6y-vU";
+const AUTH_URL = "http://20.207.122.201/evaluation-service/auth";
+const LOG_URL = "http://20.207.122.201/evaluation-service/logs";
+
+let TOKEN = "";
+
+// 🔑 function to get fresh token automatically
+async function getToken() {
+  try {
+    const res = await axios.post(AUTH_URL, {
+      email: "rijitghosh53@gmail.com",
+      name: "Rijit Ghosh",
+      mobileNo: "7501897104",
+      githubUsername: "RijitGhosh",
+      rollNo: "RA2311033010017",
+      accessCode: "QkbpxH",
+      clientID: "8ffcd0f3-4256-4e61-9671-505d0b0f8c85",
+      clientSecret: "qAwfZAqHqZVfXnxV"
+    });
+
+    TOKEN = res.data.access_token;
+  } catch (err) {
+    console.log("Token fetch failed");
+  }
+}
+
 async function Log(stack, level, pkg, message) {
   try {
+    // always get fresh token before logging
+    if (!TOKEN) {
+      await getToken();
+    }
+
     const res = await axios.post(
-      "http://20.207.122.201/evaluation-service/logs",
+      LOG_URL,
       { stack, level, package: pkg, message },
       {
         headers: {
